dffa9f7cb9709dfbbf704f1052916b0738c950ac
fabioune <fabien.bauculat@gmail.com>
2019-03-20 10:03:59 +0100

    Basic sets of tests

---
+++ tests/Controllers/DockerTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Controllers;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Controller\DockerController;
+use Symfony\Component\HttpFoundation\Request;
+
+class DockerTest extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function setUp()
+    {
+        $this->client = self::createClient();
+
+        parent::setUp();
+    }
+
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testResponses(string $url)
+    {
+        $this->client->request('GET', $url);
+
+        return $this->assertJson(
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function urlProvider()
+    {
+        yield ['/docker'];
+        yield ['/docker/health'];
+    }
+
+
+}
---
+++ tests/Controllers/SteamTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Controllers;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Controller\DockerController;
+use Symfony\Component\HttpFoundation\Request;
+
+class SteamTest extends WebTestCase
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function setUp()
+    {
+        $this->client = self::createClient();
+
+        parent::setUp();
+    }
+
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testResponses(string $url)
+    {
+        $this->client->request('GET', $url);
+
+        return $this->assertJson(
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    public function urlProvider()
+    {
+        yield ['steam/add-by-id?id=100'];
+        yield ['steam/remove-by-id?id=100'];
+    }
+
+
+}
