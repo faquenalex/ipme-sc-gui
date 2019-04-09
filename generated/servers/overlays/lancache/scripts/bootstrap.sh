20dad3d06bdc08022b999e8e7e7899fdc4c8a060
fabioune <fabien.bauculat@gmail.com>
2019-04-09 16:40:41 +0200

    rewrap

--- generated/servers/overlays/lancache/scripts/bootstrap.sh
+++ generated/servers/overlays/lancache/scripts/bootstrap.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-apt-get update;
+SCRIPT_DIR="/var/www/html"
+
+GREEN='\033[0;32m'
+BLACK='\033[0m'
+
+apt-get update
 apt-get install -y \
     zip \
     git \
@@ -16,6 +21,8 @@ apt-get install -y \
     php \
     expect \
     lib32gcc1 \
+    lib32tinfo5 \
+    lib32stdc++6 \
     ;
 
 a2enmod rewrite;
@@ -41,31 +48,37 @@ cat > /etc/apache2/sites-available/000-default.conf << EOF
 </VirtualHost>
 EOF
 
-rm -rf /var/www/html/*
+rm -rf /var/www/html/
 
-mkdir -p /var/www/html/;
-git clone https://github.com/Fabioune/lancache-autofill.git /var/www/html;
+mkdir -p /var/www/html/
+git clone https://github.com/Fabioune/lancache-autofill.git /var/www/html
 
-cd /var/www/html && ./install.sh;
+printf "${GREEN}Installing dependencies with Composer${BLACK}\n"
+cd $SCRIPT_DIR && composer update
 
+printf "${GREEN}Installing Steam${BLACK}\n"
+mkdir -p /usr/games/steam && cd /usr/games/steam && curl -sqL "http://media.steampowered.com/client/steamcmd_linux.tar.gz" | tar zxvf -
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+php $SCRIPT_DIR/lancache-autofill app:initialise-database
+
+php $SCRIPT_DIR/lancache-autofill steam:update-app-list
+
+php $SCRIPT_DIR/lancache-autofill steam:authorise-account $DEFAULT_STEAM_USER
 
 printf "${GREEN}Creating database file${BLACK}\n"
 cd $SCRIPT_DIR && touch "database.sqlite"
 
 printf "${GREEN}Creating your enviroment file${BLACK}\n"
 
-echo > ".env";
-echo DOWNLOADS_DIRECTORY="$DOWNLOADS_DIRECTORY" >> ".env";
-echo STEAMCMD_PATH="$STEAMCMD_PATH" >> ".env";
-echo DEFAULT_STEAM_USER="$DEFAULT_STEAM_USER" >> ".env";
+echo > ".env"
+echo DOWNLOADS_DIRECTORY="$DOWNLOADS_DIRECTORY" >> ".env"
+echo STEAMCMD_PATH="$STEAMCMD_PATH" >> ".env"
+echo DEFAULT_STEAM_USER="$DEFAULT_STEAM_USER" >> ".env"
 
 
-chown www-data:www-data -R /var/www/html;
-chmod 0777 -R /var/www/html/*;
+chown www-data:www-data -R /var/www/html
+chmod 0777 -R /var/www/html/
 
-service apache2 restart;
+service apache2 restart
 
-tail -f /var/log/apache2/error.log /var/log/apache2/access.log;
-
+tail -f /var/log/apache2/error.log /var/log/apache2/access.log
